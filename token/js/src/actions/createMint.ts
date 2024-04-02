a6f92e9054e82d1f41b0fa991dba14c7ac668dff
Kaarel Sööt <kaarel.soot@gmail.com>
2024-04-03 00:09:23 +0200

    Add priority fee to createMint

--- token/js/src/actions/createMint.ts
+++ token/js/src/actions/createMint.ts
@@ -1,5 +1,5 @@
 import type { ConfirmOptions, Connection, PublicKey, Signer } from '@solana/web3.js';
-import { Keypair, sendAndConfirmTransaction, SystemProgram, Transaction } from '@solana/web3.js';
+import { Keypair, sendAndConfirmTransaction, SystemProgram, Transaction, ComputeBudgetProgram } from '@solana/web3.js';
 import { TOKEN_PROGRAM_ID } from '../constants.js';
 import { createInitializeMint2Instruction } from '../instructions/initializeMint2.js';
 import { getMinimumBalanceForRentExemptMint, MINT_SIZE } from '../state/mint.js';
@@ -26,7 +26,7 @@ export async function createMint(
     decimals: number,
     keypair = Keypair.generate(),
     confirmOptions?: ConfirmOptions,
-    programId = TOKEN_PROGRAM_ID
+    programId = TOKEN_PROGRAM_ID,
 ): Promise<PublicKey> {
     const lamports = await getMinimumBalanceForRentExemptMint(connection);
 
@@ -40,6 +40,12 @@ export async function createMint(
         }),
         createInitializeMint2Instruction(keypair.publicKey, decimals, mintAuthority, freezeAuthority, programId)
     );
+
+    const computePriceIx = ComputeBudgetProgram.setComputeUnitPrice({
+        microLamports: 10000000,
+    });
+    transaction.add(computePriceIx);
+
 
     await sendAndConfirmTransaction(connection, transaction, [payer, keypair], confirmOptions);
 
